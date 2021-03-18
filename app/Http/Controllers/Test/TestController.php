8e9d48af2b1237bdc3da6cf36e1267a57d66de2b
liuyu <liuyuit@aliyun.com>
2021-03-18 17:15:25 +0800

    first commit

---
+++ app/Http/Controllers/Test/TestController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Test;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Notifications\InvoicePaid;
+use Illuminate\Support\Facades\Notification;
+
+class TestController extends Controller
+{
+    public function index()
+    {
+        $user = User::find(1);
+        $user->notify(new InvoicePaid());
+
+        Notification::send($user, new InvoicePaid());
+
+        return response('hello laravel');
+    }
+}
